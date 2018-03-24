8e4f8a0e26932a89bf8b43149a8e0516b22c431d
vinsis <6437295+vinsis@users.noreply.github.com>
2018-03-24 14:47:50 +0800

    added global avg pooling + dense layer

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
 import os
 
 import loader
@@ -35,26 +36,30 @@ class ModelCNN(nn.Module):
             nn.Conv1d(15, 25, 11, stride = 3),
             nn.ReLU(True),
             nn.BatchNorm1d(25),
-            nn.Conv1d(25, 15, 11, stride = 2),
+            nn.Conv1d(25, 50, 11, stride = 2),
             nn.ReLU(True),
-            nn.BatchNorm1d(15),
-            nn.Conv1d(15, 5, 7, stride = 3),
+            nn.BatchNorm1d(50),
+            nn.Conv1d(50, 75, 7, stride = 3),
             nn.ReLU(True),
-            nn.BatchNorm1d(5),
-            nn.Conv1d(5, N, 30),
-            nn.ReLU(True)
+            nn.BatchNorm1d(75),
+            nn.AvgPool1d(26)
         )
+        self.fc = nn.Linear(75, N)
 
     def forward(self, x):
-        return torch.squeeze(self.main(x))
+        x = self.main(x)
+        x = x.view(x.size(0),-1)
+        x = self.fc(x)
+        return x
 
 model_cnn = ModelCNN()
 if use_cuda: model_cnn.cuda()
 optimizer = torch.optim.Adam(model_cnn.parameters(), lr = LR)
+scheduler = lr_scheduler.ExponentialLR(optimizer, gamma = 0.6)
 
 def train(model, epoch):
     model.train()
-    print('Learning rate is:', LR)
+    print('Learning rate is:', optimizer.param_groups[0]['lr'])
     for i, (data, target) in enumerate(train_loader):
         target = torch.squeeze(target)
         if use_cuda:
@@ -83,3 +88,10 @@ def test(model, epoch, dataloader):
         pred = output.data.max(1, keepdim = True)[1]
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
     print('Evaluation: Epoch [{}] Accuracy: [{} / {}]'.format(epoch, correct, len(dataloader.dataset)))
+
+def start_training(epochs = EPOCHS, preload_weights = None):
+    if preload_weights is not None:
+        model_cnn.load_state_dict(torch.load(preload_weights))
+    for i in range(epochs):
+        scheduler.step()
+        train(model_cnn, i)
